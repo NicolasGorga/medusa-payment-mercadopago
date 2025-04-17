00d21a44c9b635994ce678f4c8aa8391a3b23587
Nicolas Gorga <nicogorga11@gmail.com>
2025-04-17 15:38:24 -0300

    New step to update account_holder and corrections to createPayment workflow

--- src/workflows/mercadopago/steps/create-payment-method.ts
+++ src/workflows/mercadopago/steps/create-payment-method.ts
@@ -1,15 +1,24 @@
 import { CreatePaymentMethodDTO, PaymentMethodDTO } from "@medusajs/framework/types"
-import { Modules } from "@medusajs/framework/utils"
+import { ContainerRegistrationKeys, Modules } from "@medusajs/framework/utils"
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
 
 export const createPaymentMethodStepId = "create-payment-method"
 
-export const createPaymentMethodStep = createStep<CreatePaymentMethodDTO, PaymentMethodDTO, undefined>(
+export const createPaymentMethodStep = createStep<CreatePaymentMethodDTO, PaymentMethodDTO | null, undefined>(
     createPaymentMethodStepId,
     async (data: CreatePaymentMethodDTO, { container }) => {
         const service = container.resolve(Modules.PAYMENT)
+        const logger = container.resolve(ContainerRegistrationKeys.LOGGER)
+
         
-        const paymentMethod = await service.createPaymentMethods(data)
+        let paymentMethod: PaymentMethodDTO | null = null
+        try {
+            paymentMethod = await service.createPaymentMethods(data)
+        } catch (error) {
+            // Until this pull request is merged, we catch to allow payment to continue,
+            // as the continueOnPermanentFailure config at the workflow level doesn't work: https://github.com/medusajs/medusa/pull/12027
+            logger.error(`Error occurred while trying to save payment method for provider ${data.provider_id}`, error)
+        }
 
         return new StepResponse(paymentMethod)
     }
--- src/workflows/mercadopago/workflows/create-payment.ts
+++ src/workflows/mercadopago/workflows/create-payment.ts
@@ -1,6 +1,5 @@
 import {
   useQueryGraphStep,
-  useRemoteQueryStep,
 } from "@medusajs/medusa/core-flows";
 import {
   createWorkflow,
@@ -11,10 +10,10 @@ import {
 import { PaymentCreateRequest } from "mercadopago/dist/clients/payment/create/types";
 import {
   AccountHolderDTO,
-  AdminPaymentSession,
-  CreatePaymentMethodDTO,
   CustomerDTO,
+  PaymentCustomerDTO,
   PaymentSessionDTO,
+  UpdateAccountHolderDTO,
 } from "@medusajs/framework/types";
 import {
   validateTransactionAmountStep,
@@ -22,10 +21,13 @@ import {
 } from "../steps/validate-transaction-amount";
 import { createPaymentStep } from "../steps/create-payment";
 import { createPaymentMethodStep } from "../steps/create-payment-method";
+import { udpateAccountHolderStep } from "../steps/update-account-holder";
+import { CustomerResponse } from "mercadopago/dist/clients/customer/commonTypes";
+import { PostStoreMercadopagoPaymentType } from "../../../api/store/mercadopago/payment/validators";
 
 type CreatePaymentWorkflowInput = {
   paymentSessionId: string;
-  paymentData: PaymentCreateRequest;
+  paymentData: PostStoreMercadopagoPaymentType['paymentData'];
   customerId?: string;
 };
 
@@ -56,53 +58,6 @@ export const createMercadopagoPaymentWorkflow = createWorkflow(
         paymentSessions[0] as unknown as PaymentSessionDTO
     );
 
-    when({ customerId }, ({ customerId }) => !!customerId).then(() => {
-      const customerQuery = useQueryGraphStep({
-        entity: "customer",
-        fields: [
-          "id",
-          "email",
-          "company_name",
-          "first_name",
-          "last_name",
-          "phone",
-          "addresses.*",
-          "account_holder.*",
-          "metadata",
-        ],
-        filters: { id: customerId },
-      }).config({ name: "get-customer" });
-
-
-
-      const customer: CustomerDTO & { account_holder: AccountHolderDTO } = transform({ customerQuery }, ({ customerQuery }) => {
-        return customerQuery.data[0]
-      })
-
-      const paymentCustomer = transform({ customer }, (data) => {
-        return {
-          ...data.customer,
-          billing_address:
-            data.customer.addresses?.find((a) => a.is_default_billing) ??
-            data.customer.addresses?.[0],
-        };
-      });
-
-      const paymentMethodInput = transform({ paymentSession, paymentCustomer, customer, paymentData }, ({ paymentSession, paymentCustomer, customer, paymentData }) => {
-        return {
-          provider_id: paymentSession.provider_id,
-          data: paymentData,
-          context: {
-            account_holder: customer.account_holder,
-            customer: paymentCustomer,
-            idempotency_key: paymentData.token,
-          },
-        };
-      });
-
-      createPaymentMethodStep(paymentMethodInput);
-    });
-
     const validateTransactionStepInput: ValidateTransactionStepInput =
       transform(
         { paymentSession, paymentData },
@@ -113,6 +68,85 @@ export const createMercadopagoPaymentWorkflow = createWorkflow(
       );
 
     validateTransactionAmountStep(validateTransactionStepInput);
+
+    const customerQuery = useQueryGraphStep({
+      entity: "customer",
+      fields: [
+        "id",
+        "email",
+        "company_name",
+        "first_name",
+        "last_name",
+        "phone",
+        "addresses.*",
+        "account_holders.*",
+        "metadata",
+      ],
+      filters: { id: customerId },
+    }).config({ name: "get-customer" });
+
+    const customer: (CustomerDTO & { account_holders: AccountHolderDTO[] }) | undefined = transform({ customerQuery }, ({ customerQuery }) => {
+      return customerQuery.data[0]
+    })
+
+    const paymentCustomer = transform({ customer }, (data) => {
+      return {
+        ...data.customer,
+        billing_address:
+          data.customer?.addresses?.find((a) => a.is_default_billing) ??
+          data.customer?.addresses?.[0] ?? null,
+      };
+    }) as PaymentCustomerDTO;
+
+    const accountHolderPaymentMethodsInput = transform({ paymentSession, paymentCustomer, customer, paymentData }, ({ paymentSession, paymentCustomer, customer, paymentData }) => {
+      return {
+        provider_id: paymentSession.provider_id,
+        data: paymentData,
+        context: {
+          account_holder: customer?.account_holders.filter(holder => holder.provider_id === 'pp_mercadopago_mercadopago')[0],
+          customer: paymentCustomer,
+          idempotency_key: paymentData.token,
+        },
+      };
+    });
+
+    when({ accountHolderPaymentMethodsInput, customer }, (input) => 
+      !!input.customer
+    ).then(() => {
+      createPaymentMethodStep(accountHolderPaymentMethodsInput).config({
+        // until this pull request is released, this won't continue on failure. See: https://github.com/medusajs/medusa/pull/12027
+        continueOnPermanentFailure: true,
+      });
+    })
+
+    when({ accountHolderPaymentMethodsInput }, (input) => {
+      const accountHolderData = input.accountHolderPaymentMethodsInput.context.account_holder?.data as unknown as CustomerResponse
+      const payerData = input.accountHolderPaymentMethodsInput.data.payer
+      // Only update identification when the data we get is for a non saved card
+      // saved cards will not hold identification property
+      return 'identification' in payerData && payerData.identification?.number !== accountHolderData?.identification?.number
+    }
+    ).then(() => {
+      const updateInput = transform({ accountHolderPaymentMethodsInput }, ({ accountHolderPaymentMethodsInput: { context, data, provider_id } }) => {
+        context.account_holder!.data = {
+          ...context.account_holder!.data,
+          //@ts-expect-error when condition guarantees type
+          identification: data.payer.identification
+        }
+
+        return {
+          id: context.account_holder!.id,
+          provider_id,
+          context,
+          }
+        }
+      ) as UpdateAccountHolderDTO
+
+      udpateAccountHolderStep(updateInput).config({
+        // until this pull request is released, this won't continue on failure. See: https://github.com/medusajs/medusa/pull/12027
+        continueOnPermanentFailure: true,
+      });
+    })
 
     return new WorkflowResponse(
       createPaymentStep({ paymentSessionId, paymentData })
